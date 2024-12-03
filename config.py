f8e0afc366601f11c6b5283bd3773fb69ac90ec6
Ashit-10 <95882932+Ashit-10@users.noreply.github.com>
2024-12-03 14:17:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+TELEGRAM_TOKEN = "5042954820:AAH6KmIsSAu9TMrqDtsm7K9AXIYF0cYzFlA"
+bot_id = '2114146261'
+bot_username = "The_Ultron_Robot"
+
+
+my_bot_id =5042954820
+
+log_chat = -1001716284663
+sudo_users = [1602293216]
